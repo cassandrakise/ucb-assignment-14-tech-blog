637f8deab0973392000063e75a3bb0cef679910e
Cassie Kise <cassandrakise@gmail.com>
2022-10-09 15:16:54 -0700

    updated controllers for review

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,60 +1,36 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Blog } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true; 
-
-            res.status(200).json(userData);
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
         });
+        
+        res.status(200).json(newBlog);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-router.post('/login', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
-
-        if (!userData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again'});
-            return; 
-        }
-
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res 
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+    
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog found with this id!'});
             return;
         }
 
-        req.session.save(() => {
-            req.sessionStore.user_id = userData.id;
-            req.session.logged_in = true;
-
-            req.json({ user: userData, message: 'You are now logged in!'});
-        });
-    
-    }  catch (err) {
-        res.status(400).json(err);
-    }    
-});
-
-router.post('/logout', (req,res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
