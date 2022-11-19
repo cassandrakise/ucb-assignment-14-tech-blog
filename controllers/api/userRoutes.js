3adb4e61f8121f4dbd72764822a1184c299e35f3
Cassie Kise <cassandrakise@gmail.com>
2022-11-18 17:48:01 -0800

    test 3

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -31,13 +31,6 @@ router.post('/signup', async (req, res) => {
             return; 
         }
 
-        // if (!validPassword) {
-        //     res 
-        //         .status(400)
-        //         .json({ message: 'Incorrect email or password, please try again' });
-        //     return;
-        // }
-
         req.session.save(() => {
             req.sessionStore.user_id = userData.id;
             req.session.logged_in = true;
@@ -76,8 +69,8 @@ router.post('/login', async (req, res) => {
         // req.session.save(() => {
         //     // req.sessionStore.user_id = userData.id;
         //     req.session.logged_in = true;
-        //     // res.setHeader('Content-Type', 'application/json');
-        //     // res.end(JSON.stringify({ "user": userData, "message" : "Successfully logged in!" })); // had to use "old school way" to receive any sort of request/response, which was only visible in firefox and not in chrome
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ "user": userData, "message" : "Successfully logged in!" })); // had to use "old school way" to receive any sort of request/response, which was only visible in firefox and not in chrome
             
         //     // res.json({ "user" : userData });
         //     // res.json({ "user" : "test" });
